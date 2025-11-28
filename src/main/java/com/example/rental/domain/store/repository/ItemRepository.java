7825a23d473f4e71438dd9cf0462fb7b15710491
j2iiin <jxxniie05@gmail.com>
2025-11-28 21:10:50 +0900

    feat: 물건/매장 조회 api 추가 	modified:   src/main/java/com/example/rental/domain/consign/controller/ConsignController.java 	modified:   src/main/java/com/example/rental/domain/store/controller/StoreController.java 	modified:   src/main/java/com/example/rental/domain/store/repository/ItemRepository.java 	modified:   src/main/java/com/example/rental/domain/store/service/StoreService.java

--- src/main/java/com/example/rental/domain/store/repository/ItemRepository.java
+++ src/main/java/com/example/rental/domain/store/repository/ItemRepository.java
@@ -1,14 +1,17 @@
 package com.example.rental.domain.store.repository;
 
-import com.example.rental.domain.store.entity.Item;
-import com.example.rental.domain.store.entity.ItemStatus;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import com.example.rental.domain.store.entity.Item;
+import com.example.rental.domain.store.entity.ItemStatus;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByStoreId(Long storeId);
+
+    List<Item> findByStatus(ItemStatus status);
 
     List<Item> findByStoreIdAndStatus(Long storeId, ItemStatus status);
 }
